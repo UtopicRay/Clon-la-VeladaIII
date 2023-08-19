6edb541b795d68f8097d2c5d64009c5a6dad3dab
UtopicRay <ray2021@gmail.com>
2023-08-19 19:12:05 -0400

    Animacon annadida

--- src/Components/LandindMain.jsx
+++ src/Components/LandindMain.jsx
@@ -3,7 +3,7 @@ import Fondo3 from '/src/img/FOTOSv3Mod.png'
 export function LandindMain(){
     return(
         <section className='flex relative justify-center items-center'>
-           <img className='absolute w-[48%] bottom-0 z-10' src={Fondo3} alt='Boxeadores'></img>
+           <img className='absolute w-[48%] bottom-0 z-10 animate-fade animate-once' src={Fondo3} alt='Boxeadores'></img>
             <img className='w-full h-full' src={Fondo} alt='Fondo de la velada'></img>
            </section>
     )
